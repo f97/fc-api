2442803d2161f6c4e7c68c2075d27b1cefdc72f2
Khoản Huỳnh <huynhduckhoan@Khoans-MacBook-Pro.local>
2020-03-08 18:53:47 +0700

    Update docker

---
+++ bin/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const mlabURI = 'mongodb://f97:f97@nhom9-shard-00-00-z5b08.gcp.mongodb.net:27017,nhom9-shard-00-01-z5b08.gcp.mongodb.net:27017,nhom9-shard-00-02-z5b08.gcp.mongodb.net:27017/nhom9?ssl=true&replicaSet=Nhom9-shard-0&authSource=admin&retryWrites=true';
+
+const con = mongoose.connect(mlabURI, (error) => {
+  if (error) {
+    console.log(`Error ${error}`);
+  }
+  else {
+    console.log('Connected successfully to server');
+  }
+});
+
+module.exports = con;
